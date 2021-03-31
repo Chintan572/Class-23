658a56271394ace7266e22f58c7aedea592818d3
Chintan572 <76901783+Chintan572@users.noreply.github.com>
2021-03-31 15:53:41 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,18 @@
+class Box{ 
+    constructor(x,y,width,height) {
+         var option={ 
+             restitution : 1 } 
+             this.body=Bodies.rectangle(x,y,width,height,option)
+              World.add(world,this.body)
+             } 
+             display() {
+                 var pause = this.body.position
+                 var angle = this.body.angle
+                 push();
+                 translate(pause.x,pause.y);
+                 rotate(angle);
+                 rectMode(CENTER) ;
+                 rect(0,0,this.width,this.height);
+                 pop();
+                 } 
+                }
